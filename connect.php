af9f5a8b1b7492ef1eab15cd7e23610590404817
amit-singh1 <61065217+amit-singh1@users.noreply.github.com>
2020-05-07 12:24:42 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 
- $con = mysqli_connect("shareddb-u.hosting.stackcp.net","12test-313333204e","sh10xqk35u","12test-313333204e");
+ $con = mysqli_connect("shareddb-u.hosting.stackcp.net","12test-313333204e","sh1oxqk35u","12test-313333204e");
 
             if (mysqli_connect_error()) {
         
